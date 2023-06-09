51bdf656082138bae1112fbc5ce94dccc65ab829
kdybe01 <kdybe01@gmail.com>
2023-06-09 10:15:59 +0900

    Solved algorithm problem : 버블 정렬

---
+++ src/main/java/com/codeup/etc/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.codeup.etc;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int num : arr)
+            System.out.println(num);
+        sc.close();
+    }
+}
