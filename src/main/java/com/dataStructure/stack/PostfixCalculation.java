a84e5691aff7459a1885f86b407cf5146f0eb0e1
kdybe01 <kdybe01@gmail.com>
2023-06-13 13:06:26 +0900

    Add algorithm : 후기 표기법 실습 (스택 활용) - InfixToPostfix.java   PostfixCalculation.java

---
+++ src/main/java/com/dataStructure/stack/PostfixCalculation.java
@@ -0,0 +1,42 @@
+package com.dataStructure.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class PostfixCalculation {
+
+    public void solution() throws IOException {
+        // 입력 처리
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+
+        Stack<Integer> digitStack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char token = input.charAt(i);
+
+            // 1. 숫자라면, 스택에 push
+            if (Character.isDigit(token))
+                digitStack.push(token - '0');
+//                digitStack.push(Character.digit(token, 10));
+                // 2. 숫자가 아니라면, (연산자) 스택에서 2번 pop하고 계산
+            else {
+                int popVal1 = digitStack.pop();
+                int popVal2 = digitStack.pop();
+                switch (token) {
+                    case '+' -> digitStack.push(popVal1 + popVal2);
+                    case '-' -> digitStack.push(popVal1 - popVal2);
+                    case '*' -> digitStack.push(popVal1 * popVal2);
+                    case '/' -> digitStack.push(popVal1 / popVal2);
+                    default -> throw new IllegalArgumentException("잘못된 입력");
+                }
+            }
+        }
+        System.out.println(digitStack.pop());
+    }
+
+    public static void main(String[] args) throws IOException {
+        new PostfixCalculation().solution();
+    }
+}
