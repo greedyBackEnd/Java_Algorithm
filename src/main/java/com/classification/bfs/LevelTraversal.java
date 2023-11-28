92109945a98b45073a3da67f3ab8c004a9071ce4
kdybe01 <kdybe01@gmail.com>
2023-11-28 20:35:55 +0900

    Solved algorithm problem : 이진트리 레벨탐색 (BFS) - LevelTraversal.java

---
+++ src/main/java/com/classification/bfs/LevelTraversal.java
@@ -0,0 +1,47 @@
+package com.classification.bfs;
+
+import java.util.*;
+
+public class LevelTraversal {
+
+    public static Node root;
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        BFS(root);
+    }
+
+    public static void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.add(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            System.out.print(L + " : ");
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                System.out.print(cur.data + " ");
+                if (cur.lt != null) Q.add(cur.lt);
+                if (cur.rt != null) Q.add(cur.rt);
+            }
+            L++;
+            System.out.println();
+        }
+    }
+
+    public static class Node {
+        int data;
+        Node lt, rt;
+
+        public Node(int data) {
+            this.data = data;
+            this.lt = this.rt = null;
+        }
+    }
+}
